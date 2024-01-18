36b9a52913e1f422f8536fe4d5e595a5f0a100fe
breno.mcosta <breno.mcosta@pbh.gov.br>
2024-01-18 15:14:17 -0300

    up

---
+++ src/main/java/com/br/inventory/InventoryKafkaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.br.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryKafkaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryKafkaServiceApplication.class, args);
+	}
+
+}
